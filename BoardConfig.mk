0aeee5a6f32d5980175482eb143c357cde976912
Nathan Prevette <nprevette420@gmail.com>
2015-04-10 15:31:16 -0400

    Add Bliss Configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,4 +26,17 @@ TARGET_RECOVERY_FSTAB = device/asus/flo/fstab.flo
 
 include device/asus/flo/BoardConfigCommon.mk
 
-Bliss_Build_Block :=1
+# BlissPop Config Flags
+TARGET_TC_ROM := 4.8
+TARGET_TC_KERNEL := 4.8
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := false
+BLISS_GRAPHITE := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
